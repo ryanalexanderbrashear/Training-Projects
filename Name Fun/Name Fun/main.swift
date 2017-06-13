4928987ea03712ffc7b1547eb38140f2eab73ee5
ryanbrashear <ryan.brashear@interapthq.com>
2017-06-13 15:56:15 -0400

    created the initial project

---
+++ Name Fun/Name Fun/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Name Fun
+//
+//  Created by Ryan Brashear on 6/13/17.
+//  Copyright © 2017 Ryan Brashear. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
