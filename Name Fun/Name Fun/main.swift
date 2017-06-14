64d9b5205af7db2bffb6311ad0b2b90ac23860e3
ryanbrashear <ryan.brashear@interapthq.com>
2017-06-14 10:56:47 -0400

    added in functionality for reversing the name the user enters and displaying the result

--- Name Fun/Name Fun/main.swift
+++ Name Fun/Name Fun/main.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 
+/*********************** 1 ***********************/
+
 //Function to get user input from the keyboard
 func getUserInput() -> String {
     let keyboard = FileHandle.standardInput
@@ -20,6 +22,30 @@ func getUserInput() -> String {
     }
 }
 
+//Prompt to request the user enters their name
 print("Please enter your name: ")
+
+//Call the function to get the user input from the keyboard and assign it to a constant
 let name = getUserInput()
+
+/*********************** 2 ***********************/
+
+//Create an empty array of characters used to store each character in our name string as we break it apart
+var characterArray: [Character] = []
+
+//Add each character in the name string to our character array
+for character in name.characters {
+    characterArray.append(character)
+}
+
+//Create an empty string which we will use to hold our reversed name string
+var reversedName = ""
+
+//Go through our character array in reverse, and add each character to our reversed name string
+for index in stride(from: characterArray.count, to: 0, by: -1) {
+    reversedName.append(characterArray[index-1])
+}
+
+//Finally, print out the reversed name
+print(reversedName)
 
