6bbae13f15585ad252defa2d265c0a13b4df059d
ryanbrashear <ryan.brashear@interapthq.com>
2017-06-15 13:54:36 -0400

    added in the Concert data model

---
+++ Concert Lister/Concert Lister/Concert.swift
@@ -0,0 +1,17 @@
+//
+//  Concert.swift
+//  Concert Lister
+//
+//  Created by Ryan Brashear on 6/15/17.
+//  Copyright © 2017 Ryan Brashear. All rights reserved.
+//
+
+import Foundation
+
+class Concert {
+    var title: String
+    
+    init(title: String) {
+        self.title = title
+    }
+}
