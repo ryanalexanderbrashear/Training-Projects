d680a18150f4a4909a9451d1b6dbd09499f57db6
ryanbrashear <ryan.brashear@interapthq.com>
2017-06-15 21:10:36 -0400

    added in functionality for selecting menu options, the person data model, and a function to allow the user to add a new name

---
+++ Attendance Roll Call/Attendance Roll Call/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  Attendance Roll Call
+//
+//  Created by Ryan Brashear on 6/15/17.
+//  Copyright © 2017 Ryan Brashear. All rights reserved.
+//
+
+import Foundation
+
+class Person {
+    var firstName: String
+    var lastName: String
+    
+    init(firstName: String, lastName: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+}
--- Attendance Roll Call/Attendance Roll Call/main.swift
+++ Attendance Roll Call/Attendance Roll Call/main.swift
@@ -8,12 +8,52 @@
 
 import Foundation
 
+var finished: Bool = false
+
+let inputManager = InputManager()
+
+var people: [Person] = []
+
 func showMainMenu() {
     print("MAIN MENU")
     print("1. Add a name")
     print("2. See all names")
     print("3. Look up a name")
     print("4. Remove a name")
+    print("5. Quit")
 }
 
-showMainMenu()
+func addName() {
+    print("Please enter the person's first name: ")
+    let inputFirstName = inputManager.getUserInput()
+    print("Please enter the person's last name: ")
+    let inputLastName = inputManager.getUserInput()
+    let newPerson = Person(firstName: inputFirstName, lastName: inputLastName)
+    people.append(newPerson)
+}
+
+func selectMenuOption() {
+    if let userSelection = Int(inputManager.getUserInput()) {
+        switch userSelection {
+        case 1:
+            addName()
+        case 2:
+            print("See all names selected")
+        case 3:
+            print("Look up a name selected")
+        case 4:
+            print("Remove a name selected")
+        case 5:
+            finished = true
+        default:
+            print("There is no corresponding menu option for that selection.")
+        }
+    } else {
+        print("Invalid input entered!")
+    }
+}
+
+while !finished {
+    showMainMenu()
+    selectMenuOption()
+}
