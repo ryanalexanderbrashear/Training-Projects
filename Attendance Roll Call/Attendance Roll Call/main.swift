68644bd8dee88d661fc584594c1a3b5782b2f211
ryanbrashear <ryan.brashear@interapthq.com>
2017-06-15 20:31:28 -0400

    added the initial project files

---
+++ Attendance Roll Call/Attendance Roll Call/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Attendance Roll Call
+//
+//  Created by Ryan Brashear on 6/15/17.
+//  Copyright © 2017 Ryan Brashear. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
