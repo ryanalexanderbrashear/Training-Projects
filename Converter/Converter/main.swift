749003ddafebf9b4f50933887361b9771462b055
ryanbrashear <ryan.brashear@interapthq.com>
2017-06-20 11:06:53 -0400

    added in the InputManager class to handle getting user input. Also set up the main application loop and the function to convert an amount of inches to feet

--- Converter/Converter/main.swift
+++ Converter/Converter/main.swift
@@ -8,5 +8,45 @@
 
 import Foundation
 
-print("Hello, World!")
+//Variable used to control the loop that runs the application
+var finished: Bool = false
 
+//Instance of the input manager that handles getting user input from the keyboard
+let inputManager = InputManager()
+
+//Function to display the main menu options for the converter
+func displayMainMenu() {
+    print("Welcome to the Converter Application: ")
+    print("!. Convert inches to feet")
+    print("2. Quit")
+}
+
+//Function to get the user's main menu option selection and carry out the appropriate operation
+func getUserSelection() {
+    if let userInput = Int(inputManager.getUserInput()) {
+        switch userInput {
+        case 1:
+            convertInchesToFeet()
+        case 2:
+            finished = true
+        default:
+            print("Invalid selection entered. Please try again.")
+        }
+    }
+}
+
+//Function to get user input for a number of inches from the keyboard and convert it to feet
+func convertInchesToFeet() {
+    print("Please enter the amount of inches you which to convert to feet: ")
+    if let userInput = Int(inputManager.getUserInput()) {
+        let feet = userInput / 12
+        let inches = userInput % 12
+        print("\(feet) feet, \(inches) inches")
+    }
+}
+
+//Main loop that handles running the application and displaying the main menu and gtting user selections until the finished variable is set to true
+while !finished {
+    displayMainMenu()
+    getUserSelection()
+}
