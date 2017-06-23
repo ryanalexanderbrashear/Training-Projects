42c375bc58f0874305dab5ba79aaa9fe6415e428
ryanbrashear <ryan.brashear@interapthq.com>
2017-06-23 15:07:12 -0400

    added the initial project files

---
+++ State Capital Testing Game/State Capital Testing Game/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  State Capital Testing Game
+//
+//  Created by Ryan Brashear on 6/23/17.
+//  Copyright © 2017 Ryan Brashear. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
