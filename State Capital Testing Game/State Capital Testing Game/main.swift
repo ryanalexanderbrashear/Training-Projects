7eaaded32efc6d0e0305037064e7434d2b19e2cb
ryanbrashear <ryan.brashear@interapthq.com>
2017-06-26 13:39:28 -0400

    added an enumeration to handle the grades. Also added code to calculate the grade and assign the appropriate grade letter using the enum

--- State Capital Testing Game/State Capital Testing Game/main.swift
+++ State Capital Testing Game/State Capital Testing Game/main.swift
@@ -13,17 +13,42 @@ let inputManager = InputManager()
 var correctStates: [String] = []
 var incorrectStates: [String] = []
 
+enum GradeLetter: String {
+    case A = "A"
+    case B = "B"
+    case C = "C"
+    case D = "D"
+    case F = "F"
+}
+
+var gradeLetter: GradeLetter
+
 for state in statesDictionary {
     let stateName = state.key
     let capitalName = state.value
     print("Please enter the state capital of \(stateName): ")
     let answer = inputManager.getUserInput().uppercased()
     if answer == capitalName {
-        print("Correct!")
+        print("Correct!\n")
         correctStates.append(stateName)
     } else {
-        print("Incorrect, the capital of \(stateName) is \(capitalName).")
+        print("Incorrect, the capital of \(stateName) is \(capitalName).\n")
         incorrectStates.append(stateName)
     }
 }
 
+let grade = (Double(correctStates.count) / Double(statesDictionary.count)) * 100
+
+if grade >= 90.0 {
+    gradeLetter = .A
+} else if grade >= 80.0 {
+    gradeLetter = .B
+} else if grade >= 70.0 {
+    gradeLetter = .C
+} else if grade >= 60.0 {
+    gradeLetter = .D
+} else {
+    gradeLetter = .F
+}
+
+print("You scored a \(grade)%. Your letter grade is \(gradeLetter).")
